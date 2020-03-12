beca6ff6759fb473daf44e6cef22034009772c11
KuaqSon <qson.ng@gmail.com>
2020-03-12 13:33:50 +0700

    Updates

---
+++ precache-manifest.c16d9a342955c3d7445ed7a5cc945734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff999bcedc50c0757831eb2ae4507f1f",
+    "url": "/instahot/index.html"
+  },
+  {
+    "revision": "69957cd811939f2a2da4",
+    "url": "/instahot/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "29478002469296428ce0",
+    "url": "/instahot/static/js/2.faa5b042.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/instahot/static/js/2.faa5b042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69957cd811939f2a2da4",
+    "url": "/instahot/static/js/main.a3b8b35a.chunk.js"
+  },
+  {
+    "revision": "23a14c627e780a1795af",
+    "url": "/instahot/static/js/runtime-main.9d513220.js"
+  }
+]);
