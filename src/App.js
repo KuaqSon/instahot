efab8c626f1fbd89e6b9090359a8e485492b01df
KuaqSon <qson.ng@gmail.com>
2020-03-12 13:32:53 +0700

    Init project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Insta Hot</h1>
+      <h5>Work!!</h5>
+    </div>
+  );
+}
+
+export default App;
